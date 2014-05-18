30297c1ccda11a45036b0591d95b5e20f4cf35e6
Guido Zockoll <gzockoll@gmail.com>
2014-05-18 06:36:08 +0200

    data container

--- create_data_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -v /opt/sonatype-work --name nexus_data busybox true
--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+DATA_CONTAINER=$(docker ps -a |grep nexus_data |wc -l)
+if [ "$DATA_CONTAINER" -ne "1" ]; then
+  docker run -v /opt/sonatype-work --name nexus_data busybox true
+fi
+
 docker run -d -p 8081:8081 --volumes-from nexus_data --name nexus gzockoll/nexus
