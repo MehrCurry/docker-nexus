ce42c109f919581c6e1819d75778cac87d0c4af1
Guido Zockoll <gzockoll@gmail.com>
2016-03-18 05:04:11 +0100

    alpine linux

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 DATA_CONTAINER=$(docker ps -a |grep nexus_data |wc -l)
 if [ "$DATA_CONTAINER" -ne "1" ]; then
-  docker run -v /opt/sonatype-work --name nexus_data busybox true
+  docker create -v /sonatype-work --name nexus_data gzockoll/nexus true
 fi
 
 docker run -d -p 8081:8081 --volumes-from nexus_data --name nexus gzockoll/nexus
